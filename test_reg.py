2db9cdd40859fb4c279dede50ee530a8675c54d8
何鸿凌 <hehl@datatang.com>
2021-03-17 13:12:35 +0800

    u

--- test_reg.py
+++ test_reg.py
@@ -77,7 +77,7 @@ def main(args):
     
     reg = MODEL.get_model(num_reg).cuda()
 
-    checkpoint = torch.load(str(experiment_dir) + '/cpks/best_model.pth')
+    checkpoint = torch.load(str(experiment_dir) + '/cps/best_model.pth')
     classifier.load_state_dict(checkpoint['model_state_dict'])
 
     with torch.no_grad():
