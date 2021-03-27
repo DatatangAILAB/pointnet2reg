e07da0ef27bf8e3919f7b2ee41c8cff6be8ee5ad
何鸿凌 <hehl@datatang.com>
2021-03-27 16:44:31 +0800

    u

---
+++ models/pointnet_reg.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch.utils.data
+import torch.nn.functional as F
+from pointnet import PointNetEncoder, feature_transform_reguliarzer
+
+class get_model(nn.Module):
+    def __init__(self, num_reg, normal_channel=True):
+        super(get_model, self).__init__()
+        self.normal_channel = normal_channel
+        self.feat = PointNetEncoder(global_feat=True, feature_transform=True, channel=channel)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k)
+        self.dropout = nn.Dropout(p=0.4)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x, trans, trans_feat = self.feat(x)
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.dropout(self.fc2(x))))
+        x = self.fc3(x)
+        return x, trans_feat
+
+class get_loss(torch.nn.Module):
+    def __init__(self):
+        super(get_loss, self).__init__()
+
+    def forward(self, pred, target, trans_feat):
+        total_loss = F.mse_loss(pred, target)
+
+        return total_loss
