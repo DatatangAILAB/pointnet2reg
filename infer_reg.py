f774191b5a8d9a016f7a37fcb45b25a70425e0d2
何鸿凌 <hehl@datatang.com>
2021-03-31 17:05:15 +0800

    u

--- infer_reg.py
+++ infer_reg.py
@@ -71,7 +71,7 @@ def main(args):
     points_tensor=torch.Tensor(points)
     points_tensor = points_tensor.transpose(2, 1)
     points_tensor = points_tensor.cuda()
-    print(points_tensor.size)
+    print(points_tensor.size())
     print(points_tensor)
     pred, _ = reg(points_tensor)
     print(pred)
